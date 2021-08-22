4ece3cf4197a16368ae25e273e9daf44e93aed74
kmgliya <85997386+kmgliya@users.noreply.github.com>
2021-08-22 23:13:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+`use strict`;
+//1
+// const obj = {
+//     name: 'Galiya',
+//     age: 16,
+// };
+// Object.prototype.calculate = function(a,b){
+//     return a + b
+// };
+// console.log(obj)
+// console.log(obj.calculate(8,9))
+
+//2
+// const user = {
+//     name: "Kate",
+//     age:20,
+//     city: 'London'
+// };
+// const Hinata = Object.create(user)
+//
+// Hinata.name = 'Hinata';
+// console.log(Object.getPrototypeOf(Hinata))
+//
+//
+// Object.setPrototypeOf(user,null)
+//
+// console.log(user)
+
